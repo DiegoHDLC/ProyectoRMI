1506107ef5b0ea6684c392072017b1a2184945fd
DiegoHDLC <dherreradelacalle@gmail.com>
2022-04-22 16:26:24 -0400

    Escaneo QR o Barras, a probar

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 void main() {
   runApp(new MaterialApp(
-    home: MyApp(),
+    home: MyButton(),
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyButton extends StatefulWidget {
+  MyButton({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    final double iconSize = 40.0;
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Stateless widget"),
-      ),
-      body: new Container(
-        alignment: Alignment.center,
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            new MyCard(
-                title: new Text("Me gusta flutter"),
-                icon: new Icon(
-                  Icons.favorite,
-                  size: iconSize,
-                )),
-            new MyCard(
-                title: new Text("Me gusta este video"),
-                icon: new Icon(
-                  Icons.thumb_up,
-                  size: iconSize,
-                )),
-          ],
-        ),
-      ),
-    );
-  }
+  State<MyButton> createState() => _MyButtonState();
 }
 
-class MyCard extends StatelessWidget {
-  final Widget title;
-  final Widget icon;
+class _MyButtonState extends State<MyButton> {
+  String _data = "";
+  _scan() async {
+    await FlutterBarcodeScanner.scanBarcode(
+            "#000000", "Cancel", true, ScanMode.BARCODE)
+        .then((value) => setState(() => _data = value));
+  }
 
-  MyCard({required this.title, required this.icon});
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      padding: const EdgeInsets.only(bottom: 1.0),
-      child: new Card(
-        child: new Container(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [this.title, this.icon],
-          ),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Stateful Widget"),
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: new Container(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new Text(
+              "Hello world",
+              style: TextStyle(fontSize: 30),
+            ),
+            ElevatedButton(
+              onPressed: () => _scan(),
+              child: Text("Escanear"),
+            ),
+            Text(_data)
+          ],
         ),
       ),
     );
