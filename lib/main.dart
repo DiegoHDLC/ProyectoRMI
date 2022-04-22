ef68b22e218ecbf98de27ebf2b1d3b1aded4a4e8
DiegoHDLC <dherreradelacalle@gmail.com>
2022-04-21 22:06:37 -0400

    Empezando proyecto RMI

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp();
+  }
+}
